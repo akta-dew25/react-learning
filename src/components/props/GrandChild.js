9913e807b0e77cc2b80c806a6de3603ff49aa979
Kiran <kiran@hutechsolutions.com>
2022-10-08 17:57:44 +0530

    props teaching files

---
+++ src/components/props/GrandChild.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GrandChild() {
+  return <div>GrandChild</div>;
+}
+
+export default GrandChild;
---
+++ src/components/props/GreatGrandChild.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GreatGrandChild() {
+  return <div>GreatCrandChild</div>;
+}
+
+export default GreatGrandChild;
