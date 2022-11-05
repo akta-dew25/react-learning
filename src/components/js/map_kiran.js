28d191e51a3071e3839226f5b37d071fba19e3bc
Kiran <kiran@hutechsolutions.com>
2022-11-05 17:03:23 +0530

    map operations by kiran added

---
+++ src/components/js/map_kiran.js
@@ -0,0 +1,41 @@
+// ES6 omiting return and {} in arrow functions
+
+const { getActiveElement } = require("@testing-library/user-event/dist/utils");
+
+let arr = [1, 2, 3, 4];
+
+// map(element,index,wholeArray)
+
+// let op=arr.map((e,i)=>{})            //with brackets,: if call back function takes more than one arg
+// let op=arr.map(e=>{})                //without brackets : when only one arg
+// let op=arr.map((e)=>{})              //with brackets and only one arg
+
+//[2,3,4,5] : when u have multiple lines in function body then u need to use {} and return inside  it
+// let op = arr.map((ele, i) => {
+//   //db
+//   //setState
+//   //getDate
+//   ele += 1;
+//   return ele;
+// });
+// console.log(op);
+
+//no {}: when one line of code which is itself u want to retun then omit {} and return keyword
+// var op = arr.map((ele, i) => ele + 1);
+// console.log(op);
+
+// when multiple statement in arrow function the u need {} and return if required
+// var op = arr.map((ele, i) => {
+//   ele = ele + 1;
+//   ele = ele + i;
+//   ele = ele * 2;
+//   return ele;
+// });
+// console.log(op);
+
+// when return;
+// function getAc() {
+//   return;
+// }
+// let op2 = getAc();
+// console.log(op2);  //UNDEFINED
