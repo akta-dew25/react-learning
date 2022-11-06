08afcc4d5d2da6db994c8d2009aec997e7f5f6fd
akta dewangan <akta.dewangan@hutechsolutions.com>
2022-11-06 08:07:32 +0530

    map changes

--- src/components/js/map.js
+++ src/components/js/map.js
@@ -1,8 +1,25 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // output
 // [1,8,9,64,25,...]
+
 // Heigher Order Function
 // function which takes function as argument
 // OR
 // function which returns function as retun value
-let result = arr.map((u, i) => {});
+
+// let result = arr.map((u, i) => {
+//   if (u % 2 === 0) {
+//     return u * u * u;
+//   } else {
+//     return u * u;
+//   }
+// });
+// console.log(result);
+
+// let result = arr.map((u) => (u % 2 === 0 ? u * u * u : u * u));
+// console.log(result);
+
+let numbers = [123, 91, 27, 28, 81, 90, 92];
+
+let divide = numbers.map((e) => (e % 3 === 0 ? e : null));
+console.log(divide);
