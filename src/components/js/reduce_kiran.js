8b928aff84c418d08b25b3124b4be74964ee4eb3
Kiran <kiranpoojary483@gmail.com>
2022-11-09 17:56:15 +0530

    Update reduce_kiran.js

--- src/components/js/reduce_kiran.js
+++ src/components/js/reduce_kiran.js
@@ -2,20 +2,29 @@
 //  we use map
 
 // NOTE: syntax
-var initialValueOfResult = 0;
-var output = [1, 2, 3].reduce((result, current) => {
-  return result + current;
-}, initialValueOfResult);
+// var initialValueOfResult = 0;
+// var output = [1, 2, 3].reduce((result, current) => {
+//   return result + current;
+// }, initialValueOfResult);
+
+// console.log(output);
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var output = numbers.reduce((result, current) => {
+  if (current % 2 === 1) {
+    return result + current;
+  } else {
+    return result;
+  }
+}, 0);
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var output = numbers.reduce((result, current) => {
+  if (current % 2 === 1) {
+    return result * current;
+  } else {
+    return result;
+  }
+}, 1);
+
 console.log(output);
-
-// let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-//return number from reduce
-
-//return string from reduce
-
-//return boolean from reduce
-
-//return object from reduce
-
-//return array from reduce
